33e39709cf3e8aaa0352b8a34ce67949b39b776e
jeethalalgada32-sudo <jeethalalgada32@gmail.com>
2026-03-26 12:57:53 +0530

    Create /api/musicdb.js file with comprehensive music recommendation database organized by mood

---
+++ api/musicdb.js
@@ -0,0 +1,31 @@
+// Music Recommendation Database organized by Mood
+
+const musicRecommendationDB = {
+    happy: [
+        { title: 'Happy', artist: 'Pharrell Williams' },
+        { title: 'Uptown Funk', artist: 'Mark Ronson ft. Bruno Mars' },
+        { title: 'Can’t Stop the Feeling!', artist: 'Justin Timberlake' },
+    ],
+    sad: [
+        { title: 'Someone Like You', artist: 'Adele' },
+        { title: 'Fix You', artist: 'Coldplay' },
+        { title: 'Hurt', artist: 'Nine Inch Nails / Johnny Cash' },
+    ],
+    relaxed: [
+        { title: 'Weightless', artist: 'Marconi Union' },
+        { title: 'Sunset Lover', artist: 'Petit Biscuit' },
+        { title: 'Ocean Eyes', artist: 'Billie Eilish' },
+    ],
+    energetic: [
+        { title: 'Titanium', artist: 'David Guetta ft. Sia' },
+        { title: 'Can’t Hold Us', artist: 'Macklemore & Ryan Lewis' },
+        { title: 'Wake Me Up', artist: 'Avicii' },
+    ],
+    romantic: [
+        { title: 'Perfect', artist: 'Ed Sheeran' },
+        { title: 'All of Me', artist: 'John Legend' },
+        { title: 'A Thousand Years', artist: 'Christina Perri' },
+    ],
+};
+
+module.exports = musicRecommendationDB;
